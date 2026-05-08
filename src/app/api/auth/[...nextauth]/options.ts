9d481d27611c7c4254159f828381c83631824663
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-08 13:48:23 +0600

    add authentication setup with NextAuth and JWT support

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,67 @@
+import bcrypt from "bcryptjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+// import dbConnect from "@/lib/db";
+// import UserModel from "@/model/UserModel";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // await dbConnect();
+
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await UserModel.findOne({ email: credentials.email });
+        if (!user) return null;
+
+        const isPasswordValid = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isPasswordValid) return null;
+
+        return {
+          id: user._id.toString(),
+          email: user.email,
+          name: user.username,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user.id;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user._id = token._id as string;
+      }
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
