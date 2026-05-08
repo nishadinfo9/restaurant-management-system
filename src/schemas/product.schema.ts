c8e8cec40858a12688f513bac84d01b5f2cce89f
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-08 21:30:05 +0600

    add product schema and CreateProductDTO for product validation

---
+++ src/schemas/product.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const ProductSchema = z.object({
+  title: z.string().min(3).max(100),
+  slug: z.string("Slug must be a string").optional(),
+  description: z.string().min(10).max(1000),
+  price: z.number().positive().finite(),
+  discountPrice: z.number().optional(),
+  categoryId: z.string().length(24),
+  stock: z.number().int().min(0),
+  isAvailable: z.boolean().default(true),
+  isFeatured: z.boolean().optional(),
+});
+
+export type CreateProductDTO = z.infer<typeof ProductSchema>;
