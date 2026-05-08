66be26baff023fda1ef2a41c3afdf1c3e404110d
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-08 21:30:18 +0600

    export Product interface in ProductModel for improved accessibility

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document } from "mongoose";
 
-interface Product extends Document {
+export interface Product extends Document {
   title: string;
   slug: string;
   description: string;
