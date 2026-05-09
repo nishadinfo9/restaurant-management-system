49dbd1f26b8806eb1de8867681e82e5c18741f35
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-09 23:13:29 +0600

    refactor: update NextAuth types to include userId and role in JWT and User interfaces

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,18 +1,25 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
+  interface User {
+    id: string;
+    role: string;
+  }
+
   interface Session {
     user: {
       _id: string;
       email: string;
       username: string;
-    };
+      role: string;
+    } & DefaultSession["user"];
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    _id: string;
+    userId: string;
+    role: string;
   }
 }
