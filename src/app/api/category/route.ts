5489d547a014ba47d9d32b5399b70d1b0edc4396
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-09 06:56:29 +0600

    feat: implement category API with create and retrieve functionality

---
+++ src/app/api/category/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { CategorySchema } from "@/schemas/category.schema";
+import {
+  createCategoryIntoDB,
+  getAllCategoriesFromDB,
+} from "@/services/category.service";
+import { ApiError } from "@/utils/ApiError";
+import { ApiResponse } from "@/utils/ApiResponse";
+
+// CREATE CATEGORY
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const result = CategorySchema.safeParse(body);
+
+    if (!result.success) {
+      throw new ApiError(400, "Validation error", result.error.flatten());
+    }
+    const category = await createCategoryIntoDB(result.data);
+
+    return NextResponse.json(
+      new ApiResponse("Category created", category, 201),
+    );
+  } catch (error: unknown) {
+    console.error(error);
+
+    return NextResponse.json(new ApiError(500, "Internal server error"));
+  }
+}
+
+// GET ALL CATEGORIES
+export async function GET() {
+  try {
+    const categories = await getAllCategoriesFromDB();
+    return NextResponse.json(
+      new ApiResponse("Categories retrieved", categories, 200),
+    );
+  } catch (error: unknown) {
+    console.error(error);
+
+    return NextResponse.json(new ApiError(500, "Internal server error"));
+  }
+}
