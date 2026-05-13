574e3eaa3cc6f1015a63165f665e66c5ecf66b0a
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-13 23:18:35 +0600

    feat: create axios instance for API calls

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,12 @@
+// lib/axios.ts
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
