44b95435f5a5c9a09154c0972191ddeb205499f3
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-18 23:37:06 +0600

    feat: add useAuth hook for managing user authentication state

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,18 @@
+import { useAuthStore } from "@/store/auth.store";
+
+export const useAuth = () => {
+  const user = useAuthStore((state) => state.user);
+
+  const loading = useAuthStore((state) => state.loading);
+
+  const setUser = useAuthStore((state) => state.setUser);
+
+  const logout = useAuthStore((state) => state.logout);
+
+  return {
+    user,
+    loading,
+    setUser,
+    logout,
+  };
+};
