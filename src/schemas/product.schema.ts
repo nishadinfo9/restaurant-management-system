feaf48bef0fb7e77544d546e3ca20789e77909d7
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-18 23:45:29 +0600

    feat: add Foods page component and update product schema to include image field

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -4,6 +4,7 @@ export const ProductSchema = z.object({
   title: z.string().min(3).max(100),
   slug: z.string("Slug must be a string").optional(),
   description: z.string().min(10).max(1000),
+  image: z.string().url().optional(),
   price: z.number().positive().finite(),
   discountPrice: z.number().optional(),
   categoryId: z.string().length(24),
