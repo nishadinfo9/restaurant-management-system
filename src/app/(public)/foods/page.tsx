e9bdf21100537aad6c9058be5f841156290d0ce5
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-19 22:47:56 +0600

    feat: update Foods page to display multiple FoodCard components

--- src/app/(public)/foods/page.tsx
+++ src/app/(public)/foods/page.tsx
@@ -1,7 +1,35 @@
+import FoodCard from "@/components/cards/FoodCard";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="mt-24 flex items-center justify-between m-10">
+      <FoodCard
+        image="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+        name="Cheese Burger"
+        category="Fast Food"
+        price={12}
+        rating={4.8}
+        deliveryTime={20}
+      />
+      <FoodCard
+        image="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+        name="Cheese Burger"
+        category="Fast Food"
+        price={12}
+        rating={4.8}
+        deliveryTime={20}
+      />
+      <FoodCard
+        image="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38"
+        name="Cheese Burger"
+        category="Fast Food"
+        price={12}
+        rating={4.8}
+        deliveryTime={20}
+      />
+    </div>
+  );
 };
 
 export default page;
